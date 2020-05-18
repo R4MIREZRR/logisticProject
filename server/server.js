4152eba7bed552d24e79b6104a281fce879d925e
Rodrigo <r4mirez.r@gmail.com>
2020-05-18 00:18:27 -0500

    Primera Subid, tiene los server configurados

--- server/server.js
+++ server/server.js
@@ -2,7 +2,6 @@ var express = require('express');
 var app = express();
 const path = require('path');
 const PORT = process.env.PORT || 5000;
-
 var app_path = '../dist/logisticProject';
 
 app.use('/', express.static(path.join(__dirname, app_path)))
